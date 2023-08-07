ad96d952379b23cb280531fe75b057f0b8e9e2ae
AbdelrahmanMahany <138822186+AbdelrahmanMahany@users.noreply.github.com>
2023-08-08 01:11:19 +0300

    Update fortune.sh

--- fortune.sh
+++ fortune.sh
@@ -4,5 +4,14 @@
 echo -e "\n~~ Fortune Teller ~~\n"
 
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+N=$(( RANDOM % 6 ))
 
-echo ${RESPONSES[5]}  
+GET_FORTUNE() {
+  echo Ask a yes or no question:
+  read QUESTION
+}
+
+GET_FORTUNE()
+
+echo ${RESPONSES[$N]}
+
